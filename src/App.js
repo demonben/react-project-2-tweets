48e6a84ae8afcd795e445867babf8c78ac419c64
Dima <benensondimitrii@gmail.com>
2022-03-05 11:16:16 +0200

    deleted commented files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,3 @@
-// import FormTweet from './components/FormTweet'
-// import ListTweets from './components/ListTweets';
-// import Loader from './components/loader';
-// import { getData } from './lib/api'
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-// import NavBar from './components/NavBar';
-// import Profile from "./components/Profile"
-// import TweetContext from "./TweetContext"
 import React, { useEffect } from 'react';
 import './App.css';
 import firebase from 'firebase/app'
@@ -60,31 +52,3 @@ function App() {
 }
 
 export default App;
- // <div className="App">
-  //       <Router>
-  //         <NavBar />
-  //         <Switch>
-  //           <Router exact path="/">
-  //             <FormTweet
-  //               isButtonIsDisable={this.state.isLoading}
-  //               dataForStorage={this.state.tweets}
-  //               addTweet={(text) => this.addTweet(text)}
-  //               addTweet={this.addTweet}
-  //               userName={this.state.userName}
-  //             // updateTweets={this.updateTweets} 
-  //             />
-  //             {this.state.isLoading && <Loader></Loader>}
-  //             <TweetContext.Provider value={this.state.tweets}>
-  //               <ListTweets
-  //               // dataForList={this.state.tweets}
-  //               ></ListTweets>
-  //             </TweetContext.Provider>
-  //           </Router>
-  //           <Router path="/profile">
-  //             <Profile
-  //             // userNameChanger={(data) => this.userNameChanger(data)}
-  //             ></Profile>
-  //           </Router>
-  //         </Switch>
-  //       </Router>
-  //     </div>
