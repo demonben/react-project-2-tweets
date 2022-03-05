ce0545d80fb2f3a031354cfb330cf590bc203f1a
Dima <benensondimitrii@gmail.com>
2022-03-05 11:23:59 +0200

    added firebase config

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ import ChatRoom from "./components/ChatRoom";
 import { useState } from "react";
 import Login from "./components/Login";
 import { AuthContext } from "./context/AuthContext";
+import firebase from "firebase/app";
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBLATOMAS_t-TLsuwsRwZJzxZe5rStXsGU",
+  authDomain: "dima-ben-project.firebaseapp.com",
+  projectId: "dima-ben-project",
+  storageBucket: "dima-ben-project.appspot.com",
+  messagingSenderId: "17483853397",
+  appId: "1:17483853397:web:1ba52acf4b0184345e6edc"
+};
+
+firebase.initializeApp(firebaseConfig)
 
 function App() {
   const [authUser, setAuthUser] = useState(null);
